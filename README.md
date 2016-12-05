03b7d4a681f627881f90049c698194b6a6bfe080
mosharrofrubel <rubel007cse@gmail.com>
2016-12-05 20:56:19 +0600

    Basic Syntax

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # MasteringJava
 Best of Java Examples for Learning
+
+# Example of Following topics
+1. HelloWorld : My first Java program
+2. Learning Basic Java Syntax
+3. Java DataTypes examples
+4. Java Variables
+5. Knowing Java Operators
---
+++ src/KnowingBasicSyntax/MySyntaxClass.java
@@ -0,0 +1,33 @@
+package KnowingBasicSyntax;
+
+/**
+ * Created by mosharrofrubel on 12/5/16.
+ */
+
+// MySyntaxClass is a java class name
+public class MySyntaxClass {
+
+    public static void main(String[] args) {
+
+        // i want to print something on console
+        System.out.println("Printing Something...");
+
+        /*
+        All Java components require names. Names used for classes, variables, and methods are called identifiers.
+         */
+
+        // Here, 'i' is  a variable name. Also an identifier.
+        int i=10;
+        // 'int' is a datatype. Another example of data type is 'String'
+        String s = "Southeast University";
+
+
+
+    }
+
+    // this is a method
+    public void my_favorite_method(){
+
+    }
+
+}
