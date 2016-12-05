c868582b691063621d1cc5ba01a14392109bd50b
mosharrofrubel <rubel007cse@gmail.com>
2016-12-05 20:17:35 +0600

    My first program

---
+++ src/MyFirstJavaProgram/MyFirstJavaClass.java
@@ -0,0 +1,10 @@
+package MyFirstJavaProgram;
+
+/**
+ * Created by mosharrofrubel on 12/5/16.
+ */
+public class MyFirstJavaClass {
+    public static void main(String[] args){
+        System.out.println("Hello World! I'm learning super cool java!");
+    }
+}
