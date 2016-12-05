de046729ed623f5bb2115ce30773fca2add345f3
mosharrofrubel <rubel007cse@gmail.com>
2016-12-06 00:23:41 +0600

    Learning Variables

---
+++ src/JavaCoolVariables/MyCoolVariable.java
@@ -0,0 +1,46 @@
+package JavaCoolVariables;
+
+/**
+ * Created by mosharrofrubel on 12/6/16.
+ */
+public class MyCoolVariable {
+
+    int year = 2016;
+    static int my_salary= 2000;
+
+    public static void main(String[] args) {
+
+        // Declaring variables
+        int i;
+        String text;
+
+        // initializing variables
+        i = 20;
+        text = "My Name is Afrin & My age is: ";
+
+        System.out.println(text+i);
+
+
+        // Understanding Variable kinds
+
+        // 'month' is a local variable
+        int month = 12;
+
+
+
+        // salary  variable is a static variable
+        System.out.println(my_salary);
+
+    }
+
+    // this is a method
+    public void my_cool_method(){
+
+        // 'year' is a Instance Variable
+        System.out.println("Year is: "+year);
+
+        // System.out.println("Year is: "+month);
+        // 'month' can't be accessed as it's a local variable
+    }
+
+}
