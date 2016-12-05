4faa1b47ed441b28a7764534c11cb5ee77cbbf6e
mosharrofrubel <rubel007cse@gmail.com>
2016-12-05 21:33:58 +0600

    Java Data Types

---
+++ src/JavaDataTypes/MyDataTypesforJava.java
@@ -0,0 +1,42 @@
+package JavaDataTypes;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+/**
+ * Created by mosharrofrubel on 12/5/16.
+ */
+public class MyDataTypesforJava {
+    public static void main(String[] args){
+
+        // Example of Primitive data types
+        boolean result = true;
+        short sho = 10;
+        char capitalC = 'C';
+        byte b = 100;
+        double d = 4.8d;
+        int i = 100000;
+        long l = 156473890;
+        float f = 9.494844f;
+        char my_plus_sign = '+';
+
+
+
+        // Reference/Object Data Types
+        String s = "Anything in text can be kept in a string variable";
+        String my_supercool_array [] = new String[20];
+
+
+
+
+        // Scanner is a java class for taking inputs from console
+        Scanner my_scan = new Scanner(System.in);
+        // Here 'my_scan' variable is an example of Reference/Object Data Type
+
+
+
+
+        // Default value of any reference variable is null.
+
+    }
+}
