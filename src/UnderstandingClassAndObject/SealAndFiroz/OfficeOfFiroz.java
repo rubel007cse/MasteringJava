24c74c980b14f95ea00ffdec2a423a1a84ed8bdc
mosharrofrubel <rubel007cse@gmail.com>
2016-12-20 12:35:13 +0600

    More class added for understanding class and object

---
+++ src/UnderstandingClassAndObject/SealAndFiroz/OfficeOfFiroz.java
@@ -0,0 +1,20 @@
+package UnderstandingClassAndObject.SealAndFiroz;
+
+/**
+ * Created by mosharrofrubel on 12/20/16.
+ */
+public class OfficeOfFiroz {
+
+    public static void main(String[] args){
+
+        // Creating Object
+        Seal obj = new Seal();
+
+        // Real Seal
+        obj.name();
+        obj.designation();
+        obj.today_date();
+
+    }
+
+}
---
+++ src/UnderstandingClassAndObject/SealAndFiroz/Seal.java
@@ -0,0 +1,20 @@
+package UnderstandingClassAndObject.SealAndFiroz;
+
+/**
+ * Created by mosharrofrubel on 12/20/16.
+ */
+public class Seal {
+
+    public void name(){
+        System.out.println("Firoz Ahemd");
+    }
+
+    public void designation(){
+        System.out.println("CEO, Alpha Tech");
+    }
+    public void today_date(){
+        System.out.println("31/12/2017");
+    }
+
+
+}
