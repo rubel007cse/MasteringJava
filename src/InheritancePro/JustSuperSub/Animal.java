1273b56dd1e27bab2e138a35baa5243f96f715ca
mosharrofrubel <rubel007cse@gmail.com>
2016-12-24 21:27:47 +0600

    more example added

---
+++ src/InheritancePro/JustSuperSub/Animal.java
@@ -0,0 +1,17 @@
+package InheritancePro.JustSuperSub;
+
+/**
+ * Created by mosharrofrubel on 12/24/16.
+ */
+public class Animal {
+
+    // this is a Super Class
+
+    void makeSound(){
+        System.out.println("Mewwww...!");
+    }
+
+    void color(){
+        System.out.println("Which and brown...!");
+    }
+}
---
+++ src/InheritancePro/JustSuperSub/Cat.java
@@ -0,0 +1,17 @@
+package InheritancePro.JustSuperSub;
+
+/**
+ * Created by mosharrofrubel on 12/24/16.
+ */
+public class Cat extends Animal {
+
+    // Subclass of Animal super class
+
+    public static void main(String[] args){
+
+        Cat c = new Cat();
+        c.makeSound();
+        c.color();
+    }
+
+}
