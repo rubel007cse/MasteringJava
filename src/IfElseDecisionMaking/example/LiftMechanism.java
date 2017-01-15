e1f7421b8e3e80a99d5a1c872ac9be16643a3e00
mosharrofrubel <rubel007cse@gmail.com>
2017-01-15 22:17:13 +0600

    if else added

---
+++ src/IfElseDecisionMaking/example/LiftMechanism.java
@@ -0,0 +1,33 @@
+package IfElseDecisionMaking.example;
+
+import java.util.Scanner;
+
+/**
+ * Created by mosharrofrubel on 1/15/17.
+ */
+public class LiftMechanism {
+    public static void main(String[] args){
+
+        System.out.println("Press 0-3 :");
+        Scanner s = new Scanner(System.in);
+        int floor_no = s.nextInt();
+
+        if(floor_no == 0) {
+            System.out.println("This is Ground Floor");
+        } else if(floor_no == 1) {
+            System.out.println("This is First Floor");
+
+        }else if(floor_no == 2) {
+            System.out.println("This is Second Floor");
+
+        }else if(floor_no == 3) {
+            System.out.println("This is Third Floor");
+
+        } else {
+            System.out.println("No more floors!");
+        }
+
+
+
+    }
+}
