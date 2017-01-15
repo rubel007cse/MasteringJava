198b334811d79e517eacaead64d460eab45cb497
mosharrofrubel <rubel007cse@gmail.com>
2017-01-15 15:24:34 +0600

    encapsulation examples added

---
+++ src/EncapsulationAndGetterSetter/examples/example2/FetchingCardDetails.java
@@ -0,0 +1,22 @@
+package EncapsulationAndGetterSetter.examples.example2;
+
+/**
+ * Created by mosharrofrubel on 1/15/17.
+ */
+public class FetchingCardDetails {
+
+    public static void main(String[] s){
+
+        SDCard sdCard = new SDCard();
+
+        sdCard.setName("ExternalMemory");
+        sdCard.setSize(16.0);
+        sdCard.setFreeSpace(9.3);
+
+
+        System.out.println("Card Name: "+sdCard.getName());
+        System.out.println("Card Size: "+sdCard.getSize());
+        System.out.println("Card Free Space: "+sdCard.getFreeSpace());
+    }
+
+}
