3e81ff3d7d102cb554dd3b202cb422e3d69dc846
mosharrofrubel <rubel007cse@gmail.com>
2017-01-15 23:49:19 +0600

    Inheritance examples added

---
+++ src/InheritancePro/example2/Lecturer.java
@@ -0,0 +1,17 @@
+package InheritancePro.example2;
+
+/**
+ * Created by mosharrofrubel on 1/15/17.
+ */
+public class Lecturer extends Department {
+
+    public static void main(String[] k){
+
+        Department obj = new Lecturer();
+
+        obj.salary();
+        obj.transport();
+
+    }
+
+}
