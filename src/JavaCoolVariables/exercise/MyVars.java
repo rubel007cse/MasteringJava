712d23628c56a052c92f2df2dd6460c64ca7de93
mosharrofrubel <rubel007cse@gmail.com>
2017-01-16 20:29:19 +0600

    Variables exercise added

---
+++ src/JavaCoolVariables/exercise/MyVars.java
@@ -0,0 +1,29 @@
+package JavaCoolVariables.exercise;
+
+/**
+ * Created by mosharrofrubel on 1/16/17.
+ */
+public class MyVars {
+
+    static int year = 2016;
+
+    public static void main(String[] o){
+
+        System.out.println("Year is: "+year);
+
+        int age = 19;
+        System.out.println("Age is: "+age);
+
+        String name = "Mosharrof Rubel";
+        System.out.println("Name is: "+name);
+
+        my_cgpa();
+
+    }
+
+    static void my_cgpa(){
+        double cg = 2.5;
+        System.out.println("CGPA is: "+cg);
+    }
+
+}
