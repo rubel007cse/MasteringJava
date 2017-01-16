92d0eb0135d61c8251989daea8743fbc4f4e8ddd
mosharrofrubel <rubel007cse@gmail.com>
2017-01-17 01:15:34 +0600

    basic syntax added

---
+++ src/KnowingBasicSyntax/exercise/SyntaxPrac.java
@@ -0,0 +1,24 @@
+package KnowingBasicSyntax.exercise;
+
+/**
+ * Created by mosharrofrubel on 1/17/17.
+ */
+public class SyntaxPrac {
+
+    public static void main(String[] args){
+
+        int i = 10;
+        String s = "Java";
+
+        System.out.println(i);
+        System.out.println(s);
+
+        name();
+
+    }
+
+    public static void name(){
+        System.out.println("Name is, Mosharrof Rubel");
+    }
+
+}
